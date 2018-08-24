0835f2cfbc83d2c428417487574b1fb2a678b511
lancelot1337 <sudhanshusah2001@gmail.com>
2018-08-24 13:59:54 +0530

    Server is up and running

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-const express = require(express);
+const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
+
+let server = app.listen(port, () => {
+    console.log(`Listening on port: ${port}`)
+});
