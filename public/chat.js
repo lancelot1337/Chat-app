699f96ff665e5521bf717cd1417bf78f0b5e694e
lancelot1337 <sudhanshusah2001@gmail.com>
2018-08-24 16:16:48 +0530

    Add socket event emitter

--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,16 @@
 // make socket connections
-var socket = io.connect('http://localhost:3000');
+const socket = io.connect('http://localhost:3000');
+
+//DOM
+const message = document.getElementById('message'),
+    handle = document.getElementById('handle'),
+    btn = document.getElementById('send'),
+    output = document.getElementById('output');
+
+//Event emitter
+btn.addEventListener('click', () => {
+    socket.emit('chat', {
+        message: message.nodeValue,
+        handle: handle.value
+    });
+});
