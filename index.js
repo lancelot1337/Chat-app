38896be2dab5e8012605413fd494eeeb4c17a010
lancelot1337 <sudhanshusah2001@gmail.com>
2018-08-24 14:04:23 +0530

    Set static files dir

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Static files
+app.use(express.static('public'));
+
+// Server setup
 let server = app.listen(port, () => {
     console.log(`Listening on port: ${port}`)
 });
